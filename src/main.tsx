367ddf014ec49ac80197c22cd8e9da03e099fe33
umarfarooq3488 <uf88605@gmail.com>
2026-03-27 17:10:09 +0500

    Update index.html title and dynamically set favicon in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 
+  import React from "react";
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
+  import faviconUrl from "./assests/favicon.png?url";
   import "./index.css";
   import "./styles/globals.css";
+
+  const existingFavicon = document.querySelector<HTMLLinkElement>('link[rel~="icon"]');
+  const faviconLink = existingFavicon ?? document.createElement("link");
+  faviconLink.rel = "icon";
+  faviconLink.type = "image/png";
+  faviconLink.href = faviconUrl;
+  if (!existingFavicon) document.head.appendChild(faviconLink);
 
   createRoot(document.getElementById("root")!).render(<App />);
   
